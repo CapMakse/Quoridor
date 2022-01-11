554ac99459d0e832dcb602ce04a0ec110080eaf4
Makse3351 <pugachev.vv1401s@gmail.com>
2022-01-11 23:33:42 +0200

    add board

---
+++ Quoridor/CoordinateButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Threading.Tasks;
+
+namespace Quoridor
+{
+    public class CoordinateButton : Button
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    public CoordinateButton() : base()
+        {
+            this.BorderBrush = new SolidColorBrush(Colors.Black);
+            this.BorderThickness = new System.Windows.Thickness(3);
+            this.Background = new SolidColorBrush(Colors.White);
+        }
+    }
+}
--- Quoridor/MainWindow.xaml.cs
+++ Quoridor/MainWindow.xaml.cs
@@ -19,9 +19,88 @@ namespace Quoridor
     /// </summary>
     public partial class MainWindow : Window
     {
+        CoordinateButton[][] figureCells = {
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9]
+            };
+        CoordinateButton[][] wallCells = {
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8]
+            }; 
+        CoordinateButton[][] horizontalWalls = {
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9],
+             new CoordinateButton[9]
+            };
+        CoordinateButton[][] verticalWalls = {
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8],
+             new CoordinateButton[8]
+            };
         public MainWindow()
         {
             InitializeComponent();
+            InitBoard();
+        }
+        void InitBoard()
+        {
+            Grid Board = FindName("Board") as Grid;
+            for (int i = 0; i < 17; i++)
+            {
+                for (int j = 0; j < 17; j++)
+                {
+                    CoordinateButton coordinateButton = new CoordinateButton();
+                    Grid.SetRow(coordinateButton, i);
+                    Grid.SetColumn(coordinateButton, j);
+                    Board.Children.Add(coordinateButton);
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            figureCells[i / 2][j / 2] = coordinateButton;
+                        }
+                        else
+                        {
+                            verticalWalls[i / 2][(j - 1) / 2] = coordinateButton;
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            horizontalWalls[(i - 1) / 2][j / 2] = coordinateButton;
+                        }
+                        else
+                        {
+                            wallCells[(i - 1) / 2][(j - 1) / 2] = coordinateButton;
+                        }
+                    }
+                }
+            }
         }
     }
 }
